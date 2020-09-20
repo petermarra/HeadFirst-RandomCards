0199829e4a1b83d72f9b3c3c292663b8a6852fce
Peter Marra <neverquit@roadrunner.com>
2020-09-20 13:53:48 -0400

    add card class and enums from previous exercise add I comparer

--- RandomCards/Program.cs
+++ RandomCards/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomCards
 {
@@ -6,7 +7,33 @@ namespace RandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+                cards.Add(card);
+                //cards.Add(new Card((Suits)random.Next(4), (Values)random.Next(1, 14)));
+            }
+            Console.WriteLine("Five random cards:");
+            PrintCards(cards);
+
+            CardComparer_byValue cardComparer = new CardComparer_byValue();
+            cards.Sort(cardComparer);
+            //cards.Sort(new CardComparer_byValue());
+
+            Console.WriteLine("\r\nThose same cards sorted:");
+            PrintCards(cards);
+            
+        }
+        public static void PrintCards(List<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
         }
     }
 }
