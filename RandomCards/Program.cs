0efd9e7c339223e6db09131da3ad3017c64550f3
Peter Marra <neverquit@roadrunner.com>
2020-09-20 12:33:41 -0400

    add Console project

---
+++ RandomCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
